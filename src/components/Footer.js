2d42820fe32e7d53532be1d57871b275b190b0f3
Shubham Mittal <shubham.mitt112@gmail.com>
2023-03-23 17:12:33 +0530

    Added Footer
    Signed-off-by: Shubham Mittal <shubham.mitt112@gmail.com>

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="bg-dark text-light py-3">
+      <p className="text-center">Copyright &copy; mytodoslist.com</p>
+    </div>
+  );
+}
+
+export default Footer;
