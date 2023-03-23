398c7a04e26bb9a48f3e3ce032ce5947f43d1466
Shubham Mittal <shubham.mitt112@gmail.com>
2023-03-23 17:12:52 +0530

    Added About page
    Signed-off-by: Shubham Mittal <shubham.mitt112@gmail.com>

---
+++ src/components/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function About() {
+  return (
+    <div className="container">
+      <h1> This is the about page</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis,
+        cumque facere? Ipsam voluptas placeat odio esse tempore quasi, quam, eos
+        natus vero velit a obcaecati consequuntur omnis error sunt tempora est
+        blanditiis reiciendis ut, enim veniam maxime corrupti sit. Ipsum
+        eligendi repellat nisi dolorum quibusdam, voluptas velit fuga quaerat
+        impedit.
+      </p>
+    </div>
+  );
+}
+
+export default About;
